bc9ef1a1f112daca696cb7ac562b09d147d2d00f
Esha Vats <56391290+eshavats@users.noreply.github.com>
2020-08-28 01:04:52 +0530

    Updates

---
+++ precache-manifest.d7c3d5e92878f94185470b84bbe0d82e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e78b52b480b05c5cd39ef4bf71f56c",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "44faffd6be807e33b4bb",
+    "url": "/Monsters/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44faffd6be807e33b4bb",
+    "url": "/Monsters/static/js/main.9fff2a31.chunk.js"
+  },
+  {
+    "revision": "99d8a27463727051162c",
+    "url": "/Monsters/static/js/runtime-main.3abc2b3c.js"
+  }
+]);
